6038b1189b2216076a573d96c6d5823351369e22
Sahil Gufran <imailsahil1@gmail.com>
2024-08-28 19:49:29 +0530

    Add files via upload

---
+++ src/routers/auth.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {Sign,Login,ForgotPassword,ResetPassword} from '../controllers/auth.js';
+
+
+const router = express.Router();
+
+router.post('/signup', Sign);
+router.post('/login', Login);
+router.post('/password/forgot',ForgotPassword);
+router.post('/password/reset',ResetPassword);
+
+export default router;
